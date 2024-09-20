91a29763077fa513eec51d9c4d8fac723c6d2bb7
PikuzaMarie <pikuza.mari@gmail.com>
2024-09-20 23:44:01 +0300

    feat: animate progress bar thumb, play song from chosen value on bar

--- main.js
+++ main.js
@@ -6,29 +6,27 @@ const playForward = document.getElementById('btn-forward');
 const img = playPauseBtn.querySelector('img');
 const endTime = document.getElementById('end');
 let isPlay = false;
-
 //Format duration time
 const formatTime = (seconds) => {
     const minutes = Math.floor(seconds / 60);
     const secs = Math.floor(seconds % 60);
     return `${minutes}:${secs < 10 ? '0' : ''}${secs}`;
 }
-
 //Set progress bar values and duration time
 song.onloadedmetadata = () => {
     progressBar.max = song.duration;
     progressBar.value = song.currentTime;
     endTime.textContent = formatTime(song.duration);
 }
-
 //Play-Pause function
-const playPause = () => {
+let time;
+const playPause = (time = 0) => {
     if(!isPlay) {
         playPauseBtn.classList.remove('play');
         playPauseBtn.classList.add('pause');
         img.src = "/assets/svg/pause_24dp_00020C_FILL0_wght400_GRAD0_opsz24.svg";
         isPlay = true;
-        song.currentTime = 0;
+        song.currentTime = time;
         song.play();
     } else {
         playPauseBtn.classList.remove('pause');
@@ -38,5 +36,17 @@ const playPause = () => {
         song.pause();
     }
 }
-
-playPauseBtn.addEventListener('click', playPause);
+//Update progress bar
+setInterval(() => {
+    if (isPlay) {
+        progressBar.value = song.currentTime;
+    }
+}, 500);
+//Play song from chosen value on progress bar
+progressBar.oninput = () => {
+    song.currentTime = progressBar.value;
+    if (!isPlay) {
+        playPause(song.currentTime);
+    }
+}
+playPauseBtn.addEventListener('click', () => playPause(song.currentTime));
