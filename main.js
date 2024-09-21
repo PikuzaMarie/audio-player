d2f93ea6ca29a8e031085403710aa866830e3fca
PikuzaMarie <pikuza.mari@gmail.com>
2024-09-21 16:44:35 +0300

    feat: add play next and previous buttons functionality, add arr with 3 song objects

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
-const progressBar = document.getElementById('progress-bar');
+const playlist = [
+    {
+        name: 'Everithing in its right place',
+        group: 'Radiohead',
+        cover: '/assets/img/cover-1.jpg',
+        source: '/assets/audio/Radiohead - Everything In Its Right Place.mp3'
+    },
+    {
+        name: 'Optimistic',
+        group: 'Radiohead',
+        cover: '/assets/img/cover-2.jpg',
+        source: '/assets/audio/Radiohead - Optimistic.mp3'
+    },
+    {
+        name: 'The national anthem',
+        group: 'Radiohead',
+        cover: '/assets/img/cover-3.jpg',
+        source: '/assets/audio/Radiohead - The National Anthem.mp3'
+    }
+]
 const song = document.getElementById('song');
+const progressBar = document.getElementById('progress-bar');
 const playPauseBtn = document.getElementById('btn-play-pause');
 const playBackward = document.getElementById('btn-backward');
 const playForward = document.getElementById('btn-forward');
@@ -21,19 +41,15 @@ song.onloadedmetadata = () => {
 //Play-Pause function
 let time;
 const playPause = (time = 0) => {
-    if(!isPlay) {
-        playPauseBtn.classList.remove('play');
-        playPauseBtn.classList.add('pause');
+    if (!isPlay) {
         img.src = "/assets/svg/pause_24dp_00020C_FILL0_wght400_GRAD0_opsz24.svg";
-        isPlay = true;
         song.currentTime = time;
         song.play();
+        isPlay = true;
     } else {
-        playPauseBtn.classList.remove('pause');
-        playPauseBtn.classList.add('play');
         img.src = "/assets/svg/play_arrow_24dp_00020C_FILL0_wght400_GRAD0_opsz24.svg";
-        isPlay = false;
         song.pause();
+        isPlay = false;
     }
 }
 //Update progress bar
@@ -49,4 +65,41 @@ progressBar.oninput = () => {
         playPause(song.currentTime);
     }
 }
+//Play next song
+let playNum = 0;
+let playlistLength = playlist.length;
+const playNext = () => {
+    if (playNum < playlistLength) {
+        playNum += 1;
+    } else {
+        playNum = 0;
+    }
+    changeTrack(playlist[playNum]);
+    playPause();
+}
+//Play previous song
+const playPrev = () => {
+    if (playNum === 0) {
+        playNum = playlistLength - 1;
+    } else {
+        playNum -= 1;
+    }
+    changeTrack(playlist[playNum]);
+    playPause();
+}
+//Change track
+const changeTrack = (track) => {
+    const name = document.getElementById('name');
+    const group = document.getElementById('group');
+    const cover = document.getElementById('cover');
+
+    song.currentSrc = track.source;
+    name.textContent = track.name;
+    group.textContent = track.group;
+    cover.src = track.cover;
+    cover.alt = `${track.name} cover`;
+}
+//Add event listeners to controls
 playPauseBtn.addEventListener('click', () => playPause(song.currentTime));
+playForward.addEventListener('click', () => playNext());
+playBackward.addEventListener('click', () => playPrev());
